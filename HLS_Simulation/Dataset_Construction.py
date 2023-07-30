4b0bcb4163aad6d42007c8d69008a389b8350848
李泽颖 <139617474+leeziy@users.noreply.github.com>
2023-07-30 17:19:39 +0800

    Update Dataset_Construction.py

--- HLS_Simulation/Dataset_Construction.py
+++ HLS_Simulation/Dataset_Construction.py
@@ -2,15 +2,10 @@ import numpy
 import sys
 import tensorflow as tf
 
-numpy.set_printoptions(threshold=sys.maxsize, linewidth=sys.maxsize)
-# print(sys.maxsize)
+numpy.set_printoptions(threshold=sys.maxsize, linewidth=sys.maxsize)           # 避免自动换行
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
 train_images, test_images = train_images / 255.0, test_images / 255.0
-
-# f = open(r"E:\SYSU\Project-HLS4ML\Python\cifar-10-batches-bin\data_batch_1.bin", "rb")
-# cifar10 = f.read()
-# f.close()
 
 tb_input_features = open("tb_input_features.dat", "a")
 tb_output_predictions = open("tb_output_predictions.dat", "a")
@@ -21,7 +16,7 @@ for i in range(10000):
     feature_raw = train_images[i]
     feature_raw = numpy.reshape(feature_raw, [1024, 3])
     feature = numpy.zeros(3072, dtype=numpy.float32)
-    for j in range(1024):
+    for j in range(1024):                                      # 调整格式，由RRRR GGGG BBBB改为RGB RGB RGB RGB
         feature[j * 3] = feature_raw[j][0]
         feature[j * 3 + 1] = feature_raw[j][1]
         feature[j * 3 + 2] = feature_raw[j][2]
